2c12565e72f42b05780d14aa25e6de41c4bf5a2e
Nirajan Panthee <nirajan.panthee@gmail.com>
2012-08-07 08:20:23 +0545

    analytic2

--- twitter/analytic/check.php
+++ twitter/analytic/check.php
@@ -1,34 +1,47 @@
 <?php
 include 'connection.php';
-	$test='0';
-	$date=mysql_query("SELECT DISTINCT(FROM_UNIXTIME(DateTime,'%Y,%m') FROM infotweets WHERE HashTag='#ajax'");
-	?> AJAX<table border="1">
+	$hash="#ajax";
+	$date_month=mysql_query("SELECT DISTINCT (FROM_UNIXTIME(DATETIME,'%Y,%m')) AS dates, COUNT( Tweets ) FROM infotweets WHERE HashTag='$hash' GROUP BY dates ORDER BY dates DESC");
+	?> AJAX(BY Month)<table border="1">
 	<tr><td>Date</td><td>count</td></tr><?php
 	
-while($row=mysql_fetch_array($date)){
-	var_dump($row);
-	//echo "<tr><td>2012</td>";
-	//echo "<td>".$row['0']."</td></tr>";
-	/* $timestamp=strtotime($row['DateTime']);
-	$date_day[]=Date("d M Y",$timestamp);
+while($row=mysql_fetch_array($date_month)){
 	
-	
-}
-	foreach($date_day as $p){
-		//var_dump($p);
-		if($test!=$p){
-			$test=$p;
-		$resource=mysql_query("SELECT COUNT(HashTag) FROM infotweets WHERE HashTag='#ajax' AND DateTime LIKE '%$p%'");
-		$count=mysql_fetch_array($resource);?><tr><td><?php echo $p;?></td><td><?php echo $count['0'];?></td><tr/> <?php
-	} */
+	echo "<tr><td>".$row['dates']."</td>";
+	echo "<td>".$row['COUNT( Tweets )']."</td></tr>";
 	}
-	//SELECT DISTINCT (
-/* FROM_UNIXTIME( DATETIME,  '%Y,%m' )
-) AS dates, COUNT( Tweets ) 
-FROM infotweets
-GROUP BY dates
-LIMIT 0 , 30
-	mysql_close($con); */
+	 
 
 ?>
 </table>
+BY DAY<table border="1">
+	<tr><td>Date</td><td>count</td></tr><?php
+	$date_day=mysql_query("SELECT DISTINCT (FROM_UNIXTIME(DATETIME,'%Y,%m,%d')) AS dates, COUNT( Tweets ) FROM infotweets WHERE HashTag='$hash' GROUP BY dates ORDER BY dates DESC");
+	while($row=mysql_fetch_array($date_day)){
+	
+		echo "<tr><td>".$row['dates']."</td>";
+		echo "<td>".$row['COUNT( Tweets )']."</td></tr>";
+	
+	}
+	?></table>
+	BY YEAR<table border="1">
+	<tr><td>Date</td><td>count</td></tr><?php
+	$date_day=mysql_query("SELECT DISTINCT (FROM_UNIXTIME(DATETIME,'%Y')) AS dates, COUNT( Tweets ) FROM infotweets WHERE HashTag='$hash' GROUP BY dates ORDER BY dates DESC");
+	while($row=mysql_fetch_array($date_day)){
+		//var_dump($row);
+		echo "<tr><td>".$row['dates']."</td>";
+		echo "<td>".$row['COUNT( Tweets )']."</td></tr>";
+	
+	}
+	//mysql_close($con);
+	?></table>BY WEEK<table border="1">
+	<tr><td>Date</td><td>count</td></tr><?php
+	$date_day=mysql_query("SELECT DISTINCT (FROM_UNIXTIME(DATETIME,'%Y,%m,%U')) AS dates, COUNT( Tweets ) FROM infotweets WHERE HashTag='$hash' GROUP BY dates ORDER BY dates DESC");
+	while($row=mysql_fetch_array($date_day)){
+		//var_dump($row);
+		echo "<tr><td>".$row['dates']."</td>";
+		echo "<td>".$row['COUNT( Tweets )']."</td></tr>";
+	
+	}
+	mysql_close($con);
+	?></table>
